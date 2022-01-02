f1afdb96801f5e97ab0af75fc0263d592ad92e56
OrsonCreed <71087758+OrsonCreed@users.noreply.github.com>
2022-01-02 10:57:34 +0200

    Add files via upload

--- connection_class.php
+++ connection_class.php
@@ -6,9 +6,13 @@ class myConnection{
     protected $password;
     protected $dbName; // database name
     protected $errors;
+    protected $connection_proof;
+    protected $charset;
 
     public function myConnection(){
-        $this->errors = array(NULL,false);
+        $this->errors           = array();
+        $this->connection_proof = false;
+        $this->charset          = "utf8mbb4";
     }
     public function setCredentials($serverNamePar,$userNamePar,$passwordPar,$dbNamePar){
         // Par means parameter
@@ -22,34 +26,46 @@ class myConnection{
     public function usemysqli(){
         $newConnection = new mysqli($this->servername,$this->username,$this->password,$this->dbName);
         if ($newConnection->connect_error){
-            $this->errors[0] = "connection error";// this is limitted due to not pointing the exact source of connection error
+            $this->errors[0] = "connection error";
+            // this is limitted due to not pointing the exact source of connection error
         }else{
-            $this->errors[1] = true;
+            $this->connection_proof = true;
             return $newConnection;
         }
     }
 
+    public  function usePDO(){
+      try {
+            $DataSourceName ="mysql:host=".$this->servername.";dbname=".$this->dbName.";charset:".$this->charset;
+          $pdo = new PDO($DataSourceName,$this->username,$this->password);
+          $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+          $this->connection_proof = true;
+          return $pdo;
+      } catch (PDOException $th) {
+        $this->connection_proof = false;
+          echo "connection failed!".$th->getMessage();
+      }
+    }
+
     public function checkCon(){
-        if($this->errors[1] == true){
+        if($this->connection_proof == true){
             return true;
         }else{
             return false;
         }
     }
 
+
+
 }
 
-// $con = new myConnection();
-// $con->setCredentials("localhost","bbx","password","sample");
-// if($con->checkCon()){
-//  echo "connected successfully";
-// }else{
-//     echo "failed to connect!";
-// }
-
-
-$conn = new mysqli('localhost','bbx','password','sample');
-if ($conn->connect_error) die($conn->connect_error);
-
+$con = new myConnection();
+$con->setCredentials("localhost","bbx","password","sample");
+$con->usePDO();
+if($con->checkCon()){
+ echo "connected successfully";
+}else{
+    echo "failed to connect!";
+}
 
 ?>
